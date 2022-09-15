514e44aaca11b16ccbc504f7501ce80d7429d3bc
Vadim_Zubenko <VZworkmail@yandex.ru>
2022-09-15 23:14:01 +0700

    Cond

---
+++ Example005_Cond/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter yuor name");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
